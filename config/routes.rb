012b441c23410448f7b81de6de5de11967aac1ad
Jon Sellers <jsellers@gmail.com>
2013-02-10 09:44:39 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ SampleApp::Application.routes.draw do
    match '/about',   to: 'static_pages#about'
    match '/contact', to: 'static_pages#contact'
    match '/signup',  to: 'users#new'
+   #match '/sessions', to: 'sessions#new'
+   resources :sessions, only: [:new, :create, :destroy]
 
+     match '/signin',  to: 'sessions#new'
+     match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
